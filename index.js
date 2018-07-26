d5fb33b1392320e778fae3f32fbca9394b74ba0a
Oleg Marko <olegmarko7@gmail.com>
2018-07-26 22:31:05 +0300

    Added Node router

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World !');
+
+    let myReader = null;
+
+    if (req.url === '/index') {
+        myReader = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    } else {
+        myReader = fs.createReadStream(__dirname + '/article.txt', 'utf8');
+    }
+
+    myReader.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
