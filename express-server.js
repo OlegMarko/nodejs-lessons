e7a07b5dd192a0696d0652cb4f43be198db5cc7b
Oleg Marko <olegmarko7@gmail.com>
2018-07-26 23:26:07 +0300

    Added Template manager

--- express-server.js
+++ express-server.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('ExpressJS');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/test/:id', (req, res) => {
-    res.send(req.params.id);
+    res.render('test', {
+        id: req.params.id
+    });
 });
 
 app.listen(3000);
