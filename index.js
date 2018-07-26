1f37ad173662ed52b238649015d23246cf6b21c0
Oleg Marko <olegmarko7@gmail.com>
2018-07-26 20:35:34 +0300

    Created simple server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-http
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello World !');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Server has been started on 3000 port');
